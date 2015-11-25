76942cfbd62812b1b7cbaf1ebe0efbfbe5c2bd54
5thingol <guillem.cordoba@gmail.com>
2015-11-25 14:02:52 +0100

    Moviment de la nau

---
+++ RunnerVJ/Assets/Scripts/Move.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var speed : float;
+var rotation : float;
+
+function Start () {
+	//rigidBody = this.GetComponent.<Rigidbody>();
+	//rigidBody.velocity = Vector3(0,0,shipSpeed);
+}
+
+function FixedUpdate () {
+
+/*	if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(Vector3(0,1,0)*Time.deltaTime*speed);
+	if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(Vector3(0,-1,0)*Time.deltaTime*speed);
+	if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(Vector3(-1,0,0)*Time.deltaTime*speed);
+	if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Vector3(1,0,0)*Time.deltaTime*speed);
+*/
+	var rb = GetComponent.<Rigidbody>();
+	if (Input.GetKey(KeyCode.UpArrow)) GetComponent.<Rigidbody>().AddRelativeForce(0,speed,0);
+	//this.transform.position = this.transform.position+(Vector3(0,1,0)*Time.deltaTime*speed);
+	else if (Input.GetKey(KeyCode.DownArrow)) GetComponent.<Rigidbody>().AddRelativeForce(0,-speed,0);
+	else if (Input.GetKey(KeyCode.LeftArrow)) GetComponent.<Rigidbody>().AddRelativeForce(-speed,0,0);
+	else if (Input.GetKey(KeyCode.RightArrow)) GetComponent.<Rigidbody>().AddRelativeForce(speed,0,0);
+	else {
+		var v = Vector3(-2*rb.velocity.x,-2*rb.velocity.y, 0);
+		rb.AddForce(v);
+		//rigidBody.velocity = Vector3(0,0,shipSpeed);
+		//rigidBody.angularVelocity = Vector3(0,0,shipSpeed);
+		
+	}
+	
+	if (Input.GetKey(KeyCode.Q)) GetComponent.<Rigidbody>().AddRelativeTorque(0,0,rotation);
+	else if (Input.GetKey(KeyCode.E)) GetComponent.<Rigidbody>().AddRelativeTorque(0,0,-rotation);
+	else {
+		//var v = Vector3(-2*rb.angularVelocity.x,-2*rb.velocity.y, 0);
+		//rb.AddTorque(-2*rb.angularVelocity);
+		rb.angularVelocity = Vector3.zero;
+	}
+}
